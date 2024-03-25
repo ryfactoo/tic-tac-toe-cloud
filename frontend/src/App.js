1182efe7cb05e0b4cfb25048aade29239e8e7fc4
Igor Banaszak <110424380+ryfactoo@users.noreply.github.com>
2024-03-25 21:55:37 +0100

    Lista1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import RandomGame from "./components/RandomGame";
 
-export const API_URL = "https://localhost:51056";
+const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
+const API_PORT = process.env.REACT_APP_API_PORT;
+export const API_URL = `${API_DOMAIN}:${API_PORT}`;
 
 function App() {
 
